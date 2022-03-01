8c276971611c80d74a16756acca505334080d43d
Amir Bapiri <amirbapiri@gmail.com>
2022-03-01 11:46:41 +0330

    merge list and create funtionality of products

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,14 +1,36 @@
-from rest_framework.generics import RetrieveAPIView, CreateAPIView
+from rest_framework.generics import RetrieveAPIView, CreateAPIView, ListCreateAPIView
 
 from api.serializers import ProductSerializer
 
 from .models import Product
 
 
-class ProductCreateAPIView(CreateAPIView):
+class ProductCreateAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = []
+
+    def perform_create(self, serializer):
+        """
+        content field is already set to be considered as a required field
+        so if you don't set, you will get:
+        {
+            "content": [
+                "This field is required."
+            ]
+        }
+        if you set it to an empty value:
+            {
+                "content": [
+                    "This field may not be blank."
+                ]
+            }
+        """
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content")
+        if content is None:
+            content = title
+        serializer.save()
 
 
 class ProductDetailAPIView(RetrieveAPIView):
