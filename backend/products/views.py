b8c679411edbdf4f3c4fa7adf1d0237831b62df7
Amir Bapiri <amirbapiri@gmail.com>
2022-03-02 22:21:25 +0330

    extend TokenAuthentication class to change authorization keyword

--- backend/products/views.py
+++ backend/products/views.py
@@ -9,6 +9,7 @@ from rest_framework.generics import (
 )
 
 from api.serializers import ProductSerializer
+from api.authentication import TokenAuthentication
 from products.permissions import IsStaffEditorPermissions
 
 from .models import Product
@@ -48,7 +49,7 @@ class ProductListCreateAPIView(ListCreateAPIView):
     permission_classes = [permissions.IsAdminUser, IsStaffEditorPermissions]
     authentication_classes = [
         authentication.SessionAuthentication,
-        authentication.TokenAuthentication,
+        TokenAuthentication,
     ]
 
     def perform_create(self, serializer):
