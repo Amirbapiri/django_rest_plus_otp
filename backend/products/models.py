92de91e51b285c5c8c69fe0cb60d547689a5d966
Amir Bapiri <amirbapiri@gmail.com>
2022-03-03 23:58:30 +0330

    Create custom model manager for Product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,8 +1,30 @@
+from django.db.models import Q
 from django.db import models
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
+
+
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(is_public=True)
+
+    def search(self, query, user=None):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_public().filter(lookup)
+        if user is not None:
+            qs2 = qs.filter(user=user).filter(lookup)
+            qs = (qs | qs2).distinct()
+        return qs
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def search(self, query, user=None):
+        return self.get_queryset().search(query, user)
 
 
 class Product(models.Model):
@@ -17,6 +39,8 @@ class Product(models.Model):
     content = models.TextField()
     price = models.DecimalField(max_digits=15, decimal_places=2, default=00.00)
     is_public = models.BooleanField(default=True)
+
+    objects = ProductManager()
 
     @property
     def sale_price(self):
