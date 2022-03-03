ff2d7e7eb2e1061caf229dee0b730b09f80970b9
Amir Bapiri <amirbapiri@gmail.com>
2022-03-03 13:37:09 +0330

    display other products of a user

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework import serializers
 
 
+class UserProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name="api:products:product_detail",
+        lookup_field="pk",
+        read_only=True,
+    )
+    title = serializers.CharField(read_only=True)
+
+
 class UserPublicSerializer(serializers.Serializer):
     username = serializers.CharField(read_only=True)
+    id = serializers.IntegerField(read_only=True)
+    other_products = serializers.SerializerMethodField(read_only=True)
+
+    def get_other_products(self, obj):
+        products = obj.products.all()
+        return UserProductInlineSerializer(
+            products, many=True, context=self.context
+        ).data
