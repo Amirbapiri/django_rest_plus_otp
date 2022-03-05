24ee52daf947151cad12e85199857c0341986be1
Amir Bapiri <amirbapiri@gmail.com>
2022-03-05 11:53:46 +0330

    add model 'User' to admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ["username", "phone", "is_active"]
+    list_editable = ["is_active"]
+    list_filter = ["is_active"]
