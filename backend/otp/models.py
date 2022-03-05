e156141d1dae7a648fd992f3ed6ae9ea2be216f7
Amir Bapiri <amirbapiri@gmail.com>
2022-03-05 23:38:42 +0330

    modify Otp model

--- backend/otp/models.py
+++ backend/otp/models.py
@@ -1,7 +1,13 @@
 from uuid import uuid4
-from django.db import models
 
+from django.db import models
+from django.db.models.functions import Now
 from django.contrib.auth import get_user_model
+
+
+class OtpManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(created_at__lt=Now())
 
 
 class Otp(models.Model):
@@ -16,7 +22,9 @@ class Otp(models.Model):
         on_delete=models.CASCADE,
         related_name="otps",
     )
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    objects = OtpManager()
 
     def __str__(self):
         return f"{self.otp} -> ({self.user.username}, {self.user.phone})"
